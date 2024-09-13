6907eda2cc5463190da3fbaa2681061ee334ca24
jcrubiocasas <jcrubio@equinsa.es>
2024-09-13 11:17:02 +0200

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Juan Carlos Rubio Casas on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
